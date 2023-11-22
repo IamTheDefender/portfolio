4b6146e18f7da168ea42b43e489b23bb1958e162
IamTheDefender <72926871+IamTheDefender@users.noreply.github.com>
2023-11-22 18:56:24 +0530

    Update

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'IamTheDefender',
     title: "Java Developer",
     desciption:"I go by Defender, a part-time Java developer located in India. With a passion for coding, I bring a diverse skill set to the table, offering efficient solutions for various projects. From programming proficiency to dedication, I'm committed to contributing to the dynamic world of Java development.",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://i.imgur.com/XxuTEpM.png',
     resumePdf: ''
 }
